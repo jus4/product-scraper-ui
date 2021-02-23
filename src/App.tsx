bc21fd134ab2212d844485f6768639e70dc7d9b1
jussile <jussile@crasman.fi>
2021-02-23 23:54:01 +0200

    added nginx config, all styles to inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme/theme';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import ShoeProduct from './components/ShoeProduct';
 import PagePagination from './components/pagination';
 import Sidebar from './components/sidebar' 
@@ -9,14 +10,24 @@ import Footer from './components/footer'
 import {ShoeVariation, ProductFilters} from './interfaces/interfaces'
 import Grid from '@material-ui/core/Grid';
 import axios from 'axios';
-import './App.scss';
+//import './App.scss';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContent: {
+        maxWidth: '1280px',
+        margin: '0 auto',
+        width: '100%',
+    },
+  }),
+);
 function App() {
   const [shoeVariations, setShoeVariations] = useState<Array<ShoeVariation>>([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [filters, setFilters] = useState<ProductFilters>({shoeModel: 'unset', shop: 'unset'});
 
+  const classes = useStyles();
 
   useEffect( () => {
     const setShoeData = (page: Number) => {
@@ -38,7 +49,7 @@ function App() {
     <ThemeProvider theme={theme}>
     <div className="App">
       <Header />
-      <main className="main-content">
+      <main className={classes.mainContent}>
         <Grid container>
           <Grid item xs={12} md={3}  >
               <Sidebar filter={filters} setFilter={setFilters} />
